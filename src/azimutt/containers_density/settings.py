8c67db1f2472696ab64b1bb85d8cf4ec4bba386b
François Michaud <fmichaud@9fevrier.com>
2020-03-18 02:02:15 +0100

    Initialize settings helper

---
+++ src/azimutt/containers_density/settings.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Helper for loading settings.
+
+---
+This file is part of azimutt-containers-density
+Copyright (c) 2020 Deepnox.
+Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+---
+"""
+
+
+class SettingsLoader(object):
+
+    def __init__(self, settings):
+        pass
+
